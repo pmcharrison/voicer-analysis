fd9883d2d2135cc2dfd47c67cf224c2f5abff293
Peter Harrison <pmc.harrison@gmail.com>
2019-02-08 18:38:40 +0000

    Updating to match new data

--- src/1-model/functions/preprocess.R
+++ src/1-model/functions/preprocess.R
@@ -3,9 +3,9 @@ preprocess <- function(dat, par) {
     filter(pos > 1) %>% 
     slice(seq_len(par$downsample)) %>% 
     mutate(
-      chosen = as.integer(chosen),
-      dist_from_middle = abs(mean_pitch - !!par$middle),
-      dist_above_top = pmax(0, max_pitch - !!par$top),
-      dist_below_bottom = pmax(0, !!par$bottom - min_pitch)
+      chosen = as.integer(chosen)
+      # dist_from_middle = abs(mean_pitch - !!par$middle),
+      # dist_above_top = pmax(0, max_pitch - !!par$top),
+      # dist_below_bottom = pmax(0, !!par$bottom - min_pitch)
     )
 }
