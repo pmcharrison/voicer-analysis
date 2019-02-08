9b33e67bd020cc60b651ae1885866689483d1e57
Peter M C Harrison <pmc.harrison@gmail.com>
2019-02-08 17:56:16 +0000

    Refactored feature computation into the voicer package

--- src/0-analysis/0-analysis.R
+++ src/0-analysis/0-analysis.R
@@ -1,22 +1,26 @@
-library(magrittr)
 library(tidyverse)
 library(hcorp)
 library(hrep)
-library(incon)
-library(minVL)
-library(memoise)
 library(voicer)
-loadNamespace("plyr")
-library(futile.logger)
 
-for (f in list.files("src/0-analysis/functions", full.names = TRUE)) source(f)
+# for (f in list.files("src/0-analysis/functions", full.names = TRUE)) source(f)
 
-if (FALSE) {
-  # Run this manually to clear memoised function cache
-  forget(analyse_seq)
+get_corpus_features <- function(x, revoice_from) {
+  voicer::get_corpus_features(x, 
+                              revoice_from = revoice_from,
+                              min_octave = -2,
+                              max_octave = 1,
+                              dbl_change = TRUE,
+                              dbl_min = 3,
+                              dbl_max = 4)
 }
 
-df <- analyse_corpus(hcorp::bach_chorales_1)
-
 R.utils::mkdirs("output")
-saveRDS(df, "output/chord-features.rds")
+
+corpus <- hcorp::bach_chorales_1
+
+pc_chord_features <- get_corpus_features(corpus, revoice_from = "pc_chord")
+saveRDS(pc_chord_features, "output/pc-chord-features.rds")
+
+pc_set_features <- get_corpus_features(corpus, revoice_from = "pc_set")
+saveRDS(pc_set_features, "output/pc-set-features.rds")
