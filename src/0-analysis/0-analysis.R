bb5b69148a703eef170373121ccedfb4081d3870
Peter Harrison <pmc.harrison@gmail.com>
2019-02-08 23:19:37 +0000

    Simplifying feature analysis

--- src/0-analysis/0-analysis.R
+++ src/0-analysis/0-analysis.R
@@ -11,7 +11,7 @@ get_corpus_features <- function(x, revoice_from) {
                               min_octave = -2,
                               max_octave = 1,
                               dbl_change = TRUE,
-                              dbl_min = 3,
+                              dbl_min = 1,
                               dbl_max = 4)
 }
 
@@ -21,6 +21,3 @@ corpus <- hcorp::bach_chorales_1
 
 pc_chord_features <- get_corpus_features(corpus, revoice_from = "pc_chord")
 saveRDS(pc_chord_features, "output/pc-chord-features.rds")
-
-pc_set_features <- get_corpus_features(corpus, revoice_from = "pc_set")
-saveRDS(pc_set_features, "output/pc-set-features.rds")
