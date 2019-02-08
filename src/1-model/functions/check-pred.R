7509a9c7cd2a399a95798d9f83026711b429714f
Peter Harrison <pmc.harrison@gmail.com>
2019-02-08 23:28:52 +0000

    Bugfix in model checking

--- src/1-model/functions/check-pred.R
+++ src/1-model/functions/check-pred.R
@@ -8,7 +8,7 @@ check_pred <- function(coef_file, dat, mod, par) {
     linear_pred = as.numeric(as.matrix(df[, par$features]) %*%
                                as.matrix(coef[, "estimate"])))
   expect_equal(df$linear_pred, 
-               mod$linear.predictors[seq_along(linear_pred)])
+               mod$linear.predictors[seq_along(df$linear_pred)])
   
   # Check predicted probabilities
   df$exp_linear_pred <- exp(df$linear_pred)
