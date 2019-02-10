1c34114d00a058ddc3e25d424c5cb4b872f167ac
Peter M C Harrison <pmc.harrison@gmail.com>
2019-02-10 21:15:09 +0000

    Removing by_chord evaluations

--- src/1-model/1-model.R
+++ src/1-model/1-model.R
@@ -4,9 +4,11 @@ loadNamespace("voicer")
 
 for (f in list.files("src/1-model/functions", full.names = TRUE)) source(f)
 
-dat <- readRDS("output/pc-chord-features.rds") %>% dplyr::filter(id <= 5000)
+dat <- readRDS("output/pc-chord-features.rds") # %>% dplyr::filter(id <= 5000)
 moments <- get_moments(dat) %T>% write_csv("output/moments.csv")
-mod <- voicer::model_features(dat) %T>% saveRDS("output/mod.rds")
+mod <- voicer::model_features(dat, keep_model = FALSE)
+mod$eval$by_chord <- NULL
+saveRDS(mod, "output/mod.rds")
 
 if (FALSE) {
   pred_failure <- get_pred_failure(dat, mod)
