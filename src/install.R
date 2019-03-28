bf1e43baacb8fc1d811da0a840d27ec45410f877
Peter M C Harrison <pmc.harrison@gmail.com>
2019-03-28 12:46:03 +0000

    Updating installation instructions

--- src/install.R
+++ src/install.R
@@ -24,4 +24,7 @@ webshot::install_phantomjs()
 install.packages("pdftools")
 # This may fail and return some instructions on installing poppler-cpp.
 # Follow these instructions and try to install pdftools again.
+# On Linux, these commands might work, from the system terminal:
+# sudo apt-get update
+# sudo apt-get install libpoppler-cpp-dev
 
